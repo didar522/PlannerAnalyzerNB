562b6d728bf519dbbbac6de0c595d204de39e6d2
Didar <Didar@10.13.172.105>
2017-09-13 02:32:20 -0600

    analyzer input and graph issue solved...still need to check DB calculation

--- src/main/java/gui/analyzer/analyzerJpanel.java
+++ src/main/java/gui/analyzer/analyzerJpanel.java
@@ -69,6 +69,7 @@ public class analyzerJpanel extends javax.swing.JPanel {
         jLabel7 = new javax.swing.JLabel();
         jLabel8 = new javax.swing.JLabel();
         jLabel9 = new javax.swing.JLabel();
+        jComboBox1replanIntervalEnd = new javax.swing.JComboBox<>();
         jTabbedPane1 = new javax.swing.JTabbedPane();
         graph1ScrollPane = new javax.swing.JScrollPane();
         chart1JPanel = new javax.swing.JPanel();
@@ -97,7 +98,7 @@ public class analyzerJpanel extends javax.swing.JPanel {
         jLabel2.setText("Release end");
 
         jLabel3.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
-        jLabel3.setText("Replan interval");
+        jLabel3.setText("Range of replan interval");
 
         jLabel4.setFont(new java.awt.Font("Times New Roman", 1, 20)); // NOI18N
         jLabel4.setForeground(new java.awt.Color(0, 51, 255));
@@ -114,6 +115,8 @@ public class analyzerJpanel extends javax.swing.JPanel {
         jLabel9.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
         jLabel9.setText("Define release to analyze: ");
 
+        jComboBox1replanIntervalEnd.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "2", "4", "6", "8", "10", "12", "14" }));
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -121,38 +124,42 @@ public class analyzerJpanel extends javax.swing.JPanel {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addGap(55, 55, 55)
-                                .addComponent(jButton1))
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addGap(21, 21, 21)
-                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.CENTER)
-                                    .addComponent(jLabel8)
-                                    .addComponent(jLabel7)
-                                    .addComponent(jLabel6)
-                                    .addComponent(jLabel4)))
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addContainerGap()
-                                .addComponent(jLabel5)))
-                        .addGap(0, 0, Short.MAX_VALUE))
+                        .addGap(55, 55, 55)
+                        .addComponent(jButton1))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(21, 21, 21)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.CENTER)
+                            .addComponent(jLabel8)
+                            .addComponent(jLabel7)
+                            .addComponent(jLabel6)
+                            .addComponent(jLabel4)))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addContainerGap()
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jLabel5)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel9)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addComponent(jLabel3)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(jComboBox1replanInterval, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jComboBox1replanIntervalEnd, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addComponent(jLabel1)
-                                    .addComponent(jLabel2)
-                                    .addComponent(jLabel3))
+                                    .addComponent(jLabel2))
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                    .addComponent(jComboBox1replanInterval, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                    .addComponent(jXDatePicker1ReleaseStart, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                    .addComponent(jXDatePicker2releaseEnd, javax.swing.GroupLayout.PREFERRED_SIZE, 122, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addComponent(jLabel9)
-                                .addGap(0, 0, Short.MAX_VALUE)))))
-                .addContainerGap())
+                                    .addComponent(jXDatePicker2releaseEnd, javax.swing.GroupLayout.DEFAULT_SIZE, 121, Short.MAX_VALUE)
+                                    .addComponent(jXDatePicker1ReleaseStart, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -178,7 +185,8 @@ public class analyzerJpanel extends javax.swing.JPanel {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jComboBox1replanInterval, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel3))
+                    .addComponent(jLabel3)
+                    .addComponent(jComboBox1replanIntervalEnd, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(55, 55, 55)
                 .addComponent(jButton1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -264,10 +272,12 @@ public class analyzerJpanel extends javax.swing.JPanel {
         Date releaseStart = jXDatePicker1ReleaseStart.getDate();
         Date releaseEnd =jXDatePicker2releaseEnd.getDate(); 
         
-        int daysForReplan = Integer.parseInt((String)jComboBox1replanInterval.getSelectedItem()); 
+        int replanIntervalStart = Integer.parseInt((String)jComboBox1replanInterval.getSelectedItem());
+        int replanIntervalEnd = Integer.parseInt((String)jComboBox1replanIntervalEnd.getSelectedItem());
+        int gapInIntervalAnalysis=2; 
 //        int daysForReplan =2;
         try { 
-            runningAnalyzer (daysForReplan, releaseStart, releaseEnd );
+            runningAnalyzer (replanIntervalStart, replanIntervalEnd,gapInIntervalAnalysis, releaseStart, releaseEnd );
         } catch (IOException ex) {
             Logger.getLogger(analyzerJpanel.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -367,7 +377,7 @@ public class analyzerJpanel extends javax.swing.JPanel {
 	}
     
     
-    public void runningAnalyzer (int daysForReplan, Date releaseStart, Date releaseEnd ) throws IOException{
+    public void runningAnalyzer (int replanIntervalStart, int replanIntervalEnd, int gapInIntervalAnalysis, Date releaseStart, Date releaseEnd ) throws IOException{
 		
 		homeAnalyzer obj_homeAnalyzer;   
 		dataReleaseDates();
@@ -376,7 +386,7 @@ public class analyzerJpanel extends javax.swing.JPanel {
 				
 				list_resultAnalyzer= new ArrayList <resultTemplate> (); 
 				
-				for (int replanIterator=2;replanIterator<15;replanIterator=replanIterator+daysForReplan){
+				for (int replanIterator=replanIntervalStart;replanIterator<replanIntervalEnd;replanIterator=replanIterator+gapInIntervalAnalysis){
 					obj_homeAnalyzer = new homeAnalyzer (releaseStart, releaseEnd, replanIterator, allIssueData); 
 					list_resultAnalyzer.add(obj_homeAnalyzer.runPlanner());
 				}
@@ -457,6 +467,7 @@ public class analyzerJpanel extends javax.swing.JPanel {
     private javax.swing.JScrollPane graph1ScrollPane;
     private javax.swing.JButton jButton1;
     private javax.swing.JComboBox<String> jComboBox1replanInterval;
+    private javax.swing.JComboBox<String> jComboBox1replanIntervalEnd;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
